850898d0d2df22722d3cb1eab46edcf7fa84214c
Ivan LG <67765486+JustRab@users.noreply.github.com>
2025-06-09 10:08:44 -0600

    Format test

--- RabOneEngine/include/BaseApp.h
+++ RabOneEngine/include/BaseApp.h
@@ -2,33 +2,34 @@
 #include "Prerequisites.h"
 
 class
-	BaseApp {
+BaseApp {
 public:
-	BaseApp();
-	~BaseApp();
+  BaseApp();
+  ~BaseApp();
 
-	// Initializes the application by setting up the necessary resources.
-	HRESULT 
-	init();
+  // Initializes the application by setting up the necessary resources.
+  HRESULT
+  init();
 
-	// Updates de application logic each frame.
-	void 
-	update();
+  // Updates de application logic each frame.
+  void
+  update();
 
   // Renders the application content to the screen.
-	void 
-	render();
+  void
+  render();
 
   // Releases all resources and cleans up the application to free memory before exiting.
-	void destroy();
+  void 
+  destroy();
 
-	// Runs the application from the main entry point.
-	int 
-	run(HINSTANCE hInstance, 
-			HINSTANCE hPrevInstance, 
-			LPWSTR lpCmdLine, 
-			int nCmdShow, 
-			WNDPROC wndproc);
+  // Runs the application from the main entry point.
+  int
+  run(HINSTANCE hInstance,
+      HINSTANCE hPrevInstance,
+      LPWSTR lpCmdLine,
+      int nCmdShow,
+      WNDPROC wndproc);
 
 private:
 
