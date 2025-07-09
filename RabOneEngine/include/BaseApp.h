3168da895db9321b55c6d525adf9230c0358bb0d
Ivan LG <67765486+JustRab@users.noreply.github.com>
2025-07-09 15:34:35 -0600

    Refactor main app logic into BaseApp class & Device Context
    Moved all application state, initialization, update, and rendering logic from RabOneEngine.cpp into the BaseApp class. Updated BaseApp to encapsulate all engine resources and main loop, simplifying RabOneEngine.cpp to a minimal entry point. This improves code organization and maintainability by centralizing engine logic.
    Implemented all methods necessary to the Device Context class.

--- RabOneEngine/include/BaseApp.h
+++ RabOneEngine/include/BaseApp.h
@@ -1,5 +1,19 @@
 #pragma once
 #include "Prerequisites.h"
+#include "Window.h"
+#include "Device.h"
+#include "DeviceContext.h"
+#include "Texture.h"
+#include "SwapChain.h"
+#include "RenderTargetView.h"
+#include "DepthStencilView.h"
+#include "Viewport.h"
+#include "InputLayout.h"
+#include "ShaderProgram.h"
+#include "Buffer.h"
+#include "MeshComponent.h"
+#include "BlendState.h"
+#include "DepthStencilState.h"
 
 /**
  * @class BaseApp
@@ -13,11 +27,11 @@ public:
   /**
    * @brief Constructs a new BaseApp instance.
    */
-  BaseApp();
+  BaseApp() = default;
   /**
    * @brief Destroys the BaseApp instance and releases any held resources.
    */
-  ~BaseApp();
+  ~BaseApp() = default;
 
   /**
    * @brief Initializes the application by setting up the necessary resources.
@@ -61,5 +75,55 @@ public:
       WNDPROC wndproc);
 
 private:
+  //Customs
+  Window g_window;
+  Device g_device;
+  SwapChain g_swapChain;
+  DeviceContext g_deviceContext;
+  Texture g_backBuffer;
+  RenderTargetView g_renderTargetView;
+  Texture g_depthStencil;
+  DepthStencilView g_depthStencilView;
+  Viewport g_viewport;
+  ShaderProgram g_shaderProgram;
+  ShaderProgram g_shaderShadow;
+  BlendState g_shadowBlendState;
+  DepthStencilState g_shadowDepthStencilState;
 
+  // Camera Buffers
+  Buffer m_neverChanges;
+  Buffer m_changeOnResize;
+
+  // Cube Buffers
+  Buffer m_vertexBuffer;
+  Buffer m_indexBuffer;
+  Buffer m_changeEveryFrame;
+  // Cube Shadow Buffers
+  Buffer m_constShadow;
+
+  // Plane Buffers
+  Buffer m_planeVertexBuffer;
+  Buffer m_planeIndexBuffer;
+  Buffer m_constPlane;
+
+  // Variable global para el constant buffer de la luz puntual
+  ID3D11ShaderResourceView* g_pTextureRV = NULL;
+  ID3D11SamplerState* g_pSamplerLinear = NULL;
+  XMMATRIX g_World;         // Para el cubo
+  XMMATRIX g_PlaneWorld;    // Para el plano
+  XMMATRIX                            g_View;
+  XMMATRIX                            g_Projection;
+
+  //----- Variables agregadas para el plano y sombras -----//
+  UINT                                g_planeIndexCount = 0;
+
+  float ClearColor[4] = { 0.0f, 0.125f, 0.3f, 1.0f };
+  float blendFactor[4] = { 0.f, 0.f, 0.f, 0.f };
+  MeshComponent cubeMesh;
+  MeshComponent planeMesh;
+  CBNeverChanges cbNeverChanges;
+  CBChangeOnResize cbChangesOnResize;
+  CBChangesEveryFrame cbPlane;
+  CBChangesEveryFrame cb;
+  CBChangesEveryFrame cbShadow;
 };
